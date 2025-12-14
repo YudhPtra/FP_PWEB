d11c93ce02b4f910c2de76e447441e09f4d443f7
renz0412 <haenunarendra3@gmail.com>
2025-12-14 15:02:29 +0700

    Update monitoring.php

--- pages/admin/monitoring.php
+++ pages/admin/monitoring.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 'admin') {
     <title>Admin Monitoring - Task Manager</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
-    <link href="FP_PWEB/assets/css/style.css" rel="stylesheet">
+    <link href=".../.../assets/css/style.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     
     <style>
@@ -244,6 +244,6 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 'admin') {
             setInterval(checkForUpdates, 3000);
         });
     </script>
-    <script src="FP_PWEB/assets/js/script.js"></script>
+    <script src=".../.../assets/js/script.js"></script>
 </body>
 </html>
